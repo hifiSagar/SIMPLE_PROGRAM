dede0dea0a9c803857ebfaedfd290587f96f24ec
SAGAR T <ssagu6661@gmail.com>
2024-08-31 09:56:18 -0400

    Add files via upload

---
+++ simple_program.py
@@ -0,0 +1,17 @@
+sagar_vlog_kannada={
+    "subscribers" : 34,
+    "views"        : 109,
+    "likes":20 }
+print(f"sagar_vlog_kannada has a {sagar_vlog_kannada["subscribers"]} subscribers and {sagar_vlog_kannada["views"]} views and {sagar_vlog_kannada['likes']} number of likes. so for me is good thing")
+print(sagar_vlog_kannada.values())            # to see the dictinory or given values
+print("=========================")
+
+sagar_vlog_kannada["subscribers"]=35           #to change the value
+sagar_vlog_kannada["views"]=110               #to change the value
+sagar_vlog_kannada["likes"]=21             #to change the value
+print("=========================")
+
+print(f"sagar_vlog_kannada has a {sagar_vlog_kannada["subscribers"]} subscribers and {sagar_vlog_kannada["views"]} views and {sagar_vlog_kannada['likes']} number of likes. so for me is good thing")
+sagar_vlog_kannada.pop("")                 
+
+# IT HAS A SOME PROBLEM SUGGET THE SOLUTION.
